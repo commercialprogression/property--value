5044444ac5bdbdcd60c76701a31ba263028465bf
Brad Czerniak <ao5357@gmail.com>
2018-02-02 22:03:30 -0500

    Add releases including static compressed

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,41 @@
 'use strict';
 
 var gulp = require('gulp'),
+  brotli = require('gulp-brotli'),
   cleanCSS = require('gulp-clean-css'),
+  gzip = require('gulp-gzip'),
   rename = require('gulp-rename'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
   watch = require('gulp-watch');
 
+gulp.task('brotli', function () {
+  gulp.src('dist/property--value.min.css')
+    .pipe(brotli.compress({
+      extension: "br",
+      quality: 11
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
 gulp.task('css', function () {
-  return gulp.src('dist/property--value.css')
+  gulp.src('dist/property--value.css')
     .pipe(cleanCSS({
       compatibility: '*'
     }))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('gzip', function() {
+  gulp.src('dist/property--value.min.css')
+    .pipe(gzip({
+      gzipOptions: { level: 9 }
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('release', ['css', 'gzip', 'brotli']);
 
 gulp.task('sass', function() {
   gulp.src('scss/property--value.scss')
